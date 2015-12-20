b414eaf482085159fce7bebf6c898190e298231e
Melina Casanova <aury.casanova@gmail.com>
2015-12-19 22:49:13 -0700

    Added audio recording and changed rate for fast/slow buttons.

--- Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            let filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = try? AVAudioPlayer(contentsOfURL: filePathUrl)
+            audioPlayer.enableRate = true
+        } else {
+            print("The file path is empty")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +31,23 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func applySlowFilter(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
+    }
 
+    @IBAction func applyFastFilter(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.enableRate = true
+        audioPlayer.rate = 1.7
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopPlayBack(sender: UIButton) {
+        audioPlayer.stop()
+    }
+    
     /*
     // MARK: - Navigation
 

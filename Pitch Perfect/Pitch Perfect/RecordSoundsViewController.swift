5a849a26044cc6b087c7353e4f844632c7d882b7
Melina Casanova <aury.casanova@gmail.com>
2015-12-20 13:49:54 -0700

    Fixed file name so that the recording will rewrite a single file over and over rather than endlessly generate new files which we cannot delete (technically I could find a way to do so, but I am lazy and stuck in an airport).

--- Pitch Perfect/Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/Pitch Perfect/RecordSoundsViewController.swift
@@ -35,18 +35,13 @@ class RecordSoundsViewController: UIViewController {
     }
 
     @IBAction func recordAudio(sender: UIButton) {
-        //TODO: Show text "recording in progress"
-        //TODO: Record the user's voice
         recordLabel.hidden = false
         stopButton.hidden = false
         recordButton.enabled = false
         
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
         
-        let currentDateTime = NSDate()
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = "ddMMyyyy-HHmmss"
-        let recordingName = formatter.stringFromDate(currentDateTime)+".wav"
+        let recordingName = "my_audio.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = NSURL.fileURLWithPathComponents(pathArray)
         print(filePath)
